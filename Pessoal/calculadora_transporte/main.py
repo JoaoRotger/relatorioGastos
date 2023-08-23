9a79a6f53a4d9a2e5560ee6cef19b1d7f43aa8a0
joaovitorrotger <joao.rotger@edi-labs.com>
2023-08-23 13:52:18 -0300

    funcional

--- Pessoal/calculadora_transporte/main.py
+++ Pessoal/calculadora_transporte/main.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import contaDias as cd
+from openpyxl import load_workbook
+from openpyxl.styles import Font
+from openpyxl.utils import get_column_letter
+import os
+import json
+
+#Carrega o path do script e junta a string para achar o arquivo de configuração
+confsPath = os.path.join(os.path.dirname(__file__), "conf.json") 
+with open('conf.json', 'r') as file:
+  config = json.load(file)
+  valorPassagem = config["valorPassagem"]
 
 # Obtém os dias do mês
 data = cd.getDate()
@@ -10,7 +21,30 @@ tabela = pd.DataFrame({'Data': dias_do_mes, 'Motivo': 'Transporte Ida e Volta', 
 tabela = tabela[['Data', 'Motivo', 'Valor']]
 tabela = tabela.reset_index(drop=True)
 
-# Imprime o DataFrame
-print(tabela)
+# Adiciona uma linha extra ao DataFrame com o Valor Total
+valor_total = cd.quantDias(data[1], data[2]) * valorPassagem
+linha_total = pd.DataFrame({'Data': 'Valor Total', 'Motivo': '', 'Valor': valor_total}, index=[-1])
+tabela = pd.concat([tabela, linha_total]).reset_index(drop=True)
 
-tabela.to_excel(f'Relatorio_de_Gastos_{data[1]}{data[2]}.xlsx', sheet_name=f'{data[1]}{data[2]}')
+# Salva o DataFrame em uma planilha Excel
+nome_arquivo = f'Relatorio_de_Gastos_{cd.getMonthStr()}{data[2]}.xlsx'
+tabela.to_excel(nome_arquivo, sheet_name=f'{data[1]}{data[2]}', index=False)
+
+# Carrega a planilha Excel
+workbook = load_workbook(nome_arquivo)
+sheet = workbook[f'{data[1]}{data[2]}']
+
+# Ajusta a largura das colunas com base no conteúdo
+for column_cells in sheet.columns:
+    length = max(len(str(cell.value)) for cell in column_cells)
+    sheet.column_dimensions[get_column_letter(column_cells[0].column)].width = length + 2
+
+# Formatação em negrito para as células de "Valor Total" e valor
+font_negrito = Font(bold=True)
+sheet[f'C{len(tabela)+2}'].font = font_negrito
+sheet[f'D{len(tabela)+2}'].font = font_negrito
+
+# Salva as alterações na planilha Excel
+workbook.save(nome_arquivo)
+
+print(f'Planilha "{nome_arquivo}" salva com sucesso!')
